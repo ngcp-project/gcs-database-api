f88fab7468ac1c1a70ffba29840d0177bbdd2af9
Christopher Lo <christopherlo383@gmail.com>
2023-11-17 13:15:28 -0800

    deleted some files

---
+++ Models/VehicleGelocation.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+
+namespace ngcp.Models
+{
+    public class VehicleGelocation
+    {
+        public bool IsManual { get; set; } = true;
+
+        public Coordinate Target { get; set; }
+
+        //need shape class
+        public int SearchArea { get; set; }
+    }
+}
