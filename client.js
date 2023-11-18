282a31b4348f42cf14579a7dbf8d083a51c527b2
jhhocs <joshuaho1@cpp.edu>
2023-11-17 20:35:02 -0800

    Added vehicle data test

--- client.js
+++ client.js
@@ -1,39 +1,36 @@
 const WebSocket = require('ws');
 
-let counter = 0;
-
 let webSocket = new WebSocket('ws://localhost:5135/ws');
 
+// Sample Vehicle data
 let vehicleData = {
-    key: "Vehicle 1"
-    // speed: 1.1,
-    // pitch: 2.2,
-    // yaw: 3.3,
-    // roll: 4.4,
-    // altitude: 5.5,
-    // batteryLife: 6.6,
-    // lastUpdated: "00:00:00"
-    //vehicleStatus: 0
-}  
+        key: "Vehicle 2",
+        speed: 1.1,
+        pitch: 2.2,
+        yaw: 3.3,
+        roll: 4.4,
+        altitude: 5.5,
+        batteryLife: 6.6,
+        lastUpdated: "00:00:00",
+        currentPosition: {
+            latitude: 7.7,
+            longitude: 8.8
+        },
+        vehicleStatus: 0
+};      
 
-    setTimeout(() => {
-        webSocket.send(JSON.stringify(vehicleData));
-    }, 1000)
-    
+setTimeout(() => {
+    webSocket.send(JSON.stringify(vehicleData));
+}, 1000)
 
-    // setTimeout(() => {
-    //     setInterval(() => {
-    //         webSocket.send(`Current Num: ${counter++}`);
-    //     }, 1);
-    // }, 1000)    
+//let counter = 0;
+// setTimeout(() => {
+//     setInterval(() => {
+//         webSocket.send(`Current Num: ${counter++}`);
+//     }, 1);
+// }, 1000)    
 
 webSocket.addEventListener("message", (event) => {
     console.log(`Database updated: ${event.data}`);
 });
-
-// let text = '{ "employees" : [' +
-// '{ "firstName":"John" , "lastName":"Doe" },' +
-// '{ "firstName":"Anna" , "lastName":"Smith" },' +
-// '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-// webSocket.onopen = () => webSocket.send(text);
 
