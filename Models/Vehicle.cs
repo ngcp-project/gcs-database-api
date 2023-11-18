b0700563f73390bae5c155d14b66fad262077293
jhhocs <joshuaho1@cpp.edu>
2023-11-17 20:36:16 -0800

    Updated vehicle data models

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -9,14 +9,14 @@ namespace Database.Models
 
         // }
         public string key {get; set;}
-        // public float speed {get; set;}
-        // public float yaw {get; set;}
-        // public float roll {get; set;}
-        // public float altitude {get; set;}
-        // public float batteryLife {get; set;}
-        // public string lastUpdated {get; set;}
-        //public Coordinate currentPosition {get; set;}
-        //public Status_Enum vehicleStatus {get; set;}
+        public float speed {get; set;}
+        public float yaw {get; set;}
+        public float roll {get; set;}
+        public float altitude {get; set;}
+        public float batteryLife {get; set;}
+        public string lastUpdated {get; set;}
+        public Coordinate currentPosition {get; set;}
+        public Status_Enum vehicleStatus {get; set;}
 
 
         // public Vehicle() {
