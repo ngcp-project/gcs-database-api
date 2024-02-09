0972c218a4033f28e3938b1f6f1c3b03a3b6589c
christopherjlo <99621129+christopherjlo@users.noreply.github.com>
2024-02-09 14:53:00 -0800

    Zones GET endpoint (#21)
    * added skeleton for zones GET endpoint
    
    * updated env sample file
    
    * added database retrieval
    
    * modified route
    
    * updated docker-compose
    
    * modified route
    
    * fixed docker-compose file

--- Controllers/ZonesController.cs
+++ Controllers/ZonesController.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
-
+using StackExchange.Redis;
 
     public class ZonesController : ControllerBase
     {
-        [HttpGet("{keepIn}")]
+        private ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        private readonly IDatabase _redis;
+
+        public ZonesController () {
+            _redis = redis.GetDatabase();
+        }
+
+        [HttpGet("zones/{keepIn}")]
         public String getZones(string keepIn) {
             if (keepIn.ToLower() == "in") {
                 // return keep-in zones
-                return "in";
+                return _redis.StringGet("keepIn");
             }
 
             // else return keep-out zones
-            return "out";
+            if (keepIn.ToLower() == "out") {
+                // return keep-in zones
+                return _redis.StringGet("keepOut");
+            }
+
+            return "invalid zone name";
         }
+
+
     }
