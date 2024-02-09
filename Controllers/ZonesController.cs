ad6ec4b71bcf2a0532e07980b4c0f52bae7c5691
Christopher Lo <christopherlo383@gmail.com>
2024-02-09 12:53:53 -0800

    added database retrieval

--- Controllers/ZonesController.cs
+++ Controllers/ZonesController.cs
@@ -1,16 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
 
 
     public class ZonesController : ControllerBase
     {
+        private readonly IDatabase _redis;
+
+        public ZonesController (
+            IConnectionMultiplexer redis
+        ) {
+            _redis = redis.GetDatabase();
+        }
+
         [HttpGet("{keepIn}")]
         public String getZones(string keepIn) {
             if (keepIn.ToLower() == "in") {
                 // return keep-in zones
-                return "in";
+                return _redis.StringGet("keepIn");
             }
 
             // else return keep-out zones
-            return "out";
+            return _redis.StringGet("keepOut");
         }
     }
