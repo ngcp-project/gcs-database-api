27655a94098abbdf0518f45af32b2ead6cf58c30
Skyhorizon2021 <locnguyen02152021@gmail.com>
2024-03-22 13:22:25 -0700

    added post endpoint

--- Controllers/VehicleDataController.cs
+++ Controllers/VehicleDataController.cs
@@ -2,14 +2,14 @@ namespace Database.Controllers
 {
     public class VehicleDataController
     {
-        // static void Main(string[] args)
-        // {
-        //     using(var client = new HttpClient())
-        //     {
-        //         var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
-        //         var result = client.GetAsync(endpoint).Result;
-        //         var json = result.Content.ReadAsStringAsync().Result;
-        //     }
-        // }
+        static void Main(string[] args)
+        {
+            using(var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+            }
+        }
     }
 }
