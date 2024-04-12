52bd82775c9c5a5e622ec6efd5b4c8c546922c50
Skyhorizon2021 <locnguyen02152021@gmail.com>
2024-04-12 13:48:12 -0700

    added get and post endpoint for vehicle data controller

--- Controllers/VehicleDataController.cs
+++ Controllers/VehicleDataController.cs
@@ -1,15 +1,32 @@
-namespace Database.Controllers
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+
+public class VehicleDataController : ControllerBase
 {
-    public class VehicleDataController
+    private ConnectionMultiplexer conn;
+    private readonly IDatabase _redis;
+
+    public VehicleDataController()
     {
-        static void Main(string[] args)
+        conn = DBConn.Instance().getConn();
+        _redis = conn.GetDatabase();
+    }
+    [HttpGet("vehicledata/get")]
+    public String getVehicleData()
+    {
+        // return vehicle data
+        return _redis.StringGet("vehicleData");
+    }
+
+    [HttpPost("vehicledata/post")]
+    public async Task postVehicleData()
+    {
+        using (var sr = new StreamReader(Request.Body))
         {
-            using(var client = new HttpClient())
-            {
-                var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
-                var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
-            }
+            var content = await sr.ReadToEndAsync();
+            await _redis.StringSetAsync("vehicleData", content);
         }
     }
 }
