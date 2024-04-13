c4088dc243cfb3b7c7965c2cab319f7f396c5751
christopherjlo <99621129+christopherjlo@users.noreply.github.com>
2024-04-13 11:43:27 -0700

    Zones controller POST validation (#39)
    * added skeleton for zones GET endpoint
    
    * updated env sample file
    
    * added database retrieval
    
    * modified route
    
    * updated docker-compose
    
    * modified route
    
    * fixed docker-compose file
    
    * reformatted get endpoints, added post endpoints
    
    * fixed typo
    
    * changed get endpoint to only accept in or out
    
    * fixed readme formatting
    
    * added singleton class for redis connection
    
    * updated readme
    
    * implemented db singleton
    
    * fixed zonesController
    
    * Added Zone Model and started json validation in zones POST methods
    
    * started using [FromBody] parameter format
    
    * fix: convert string to ENUM when deserializing
    
    * fix: added ToString for Zone and updated Zones post method to update database
    
    * chore: updated readme
    
    * chore: delete redundant stuff in readme
    
    * fix: corrected post format
    
    ---------
    
    Co-authored-by: GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>

--- Controllers/ZonesController.cs
+++ Controllers/ZonesController.cs
@@ -1,6 +1,10 @@
-using System.Net.Http.Headers;
+using System.Reflection;
+using Database.Models;
 using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
+
+namespace Database.Controllers;
+
 
 public class ZonesController : ControllerBase
 {
@@ -28,33 +32,104 @@ public class ZonesController : ControllerBase
     }
 
     [HttpPost("zones/in")]
-    public async Task postKeepIn()
+    public async Task<IActionResult> PostKeepIn([FromBody] Zone requestBody)
     {
-        using (var sr = new StreamReader(Request.Body))
+        List<string> missingFields = new List<string>();
+
+        Type type = typeof(Zone);
+        PropertyInfo[] properties = type.GetProperties();
+
+        foreach (System.Reflection.PropertyInfo property in properties)
         {
-            string content = await sr.ReadToEndAsync();
-            await _redis.StringSetAsync("keepIn", content);
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
+
+            if (missingFields.Count > 0)
+            {
+                return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            }
+            // If any field is missing, return a bad request
         }
-    }
+        requestBody.keepIn = true;
+        if (requestBody.zoneShapeType == ShapeType.Unknown)
+        {
+            return BadRequest("Invalid shape type");
+        }
+
+
+        // await _redis.StringSetAsync("keepIn", requestBody.ToString()); // Replace "example" with the respective database key
+        await _redis.StringAppendAsync("keepIn", requestBody.ToString());
+        return Ok("Posted keepIn zone successfully.");
+    } // end postKeepIn
 
     [HttpPost("zones/out")]
-    public async Task postKeepOut()
+    public async Task<IActionResult> postKeepOut([FromBody] Zone requestBody)
     {
-        using (var sr = new StreamReader(Request.Body))
+        List<string> missingFields = new List<string>();
+
+        Type type = typeof(Zone);
+        PropertyInfo[] properties = type.GetProperties();
+
+        foreach (System.Reflection.PropertyInfo property in properties)
         {
-            string content = await sr.ReadToEndAsync();
-            await _redis.StringSetAsync("keepOut", content);
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
+
+            // If any field is missing, return a bad request
+            if (missingFields.Count > 0)
+            {
+                return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            }
+            
         }
-    }
+
+        // validate shape type
+        requestBody.keepIn = false;
+        if (requestBody.zoneShapeType == ShapeType.Unknown)
+        {
+            return BadRequest("Invalid shape type");
+        }
+
+
+        // await _redis.StringSetAsync("keepOut", requestBody.ToString()); // Replace "example" with the respective database key
+        await _redis.StringAppendAsync("keepOut", requestBody.ToString());
+        return Ok("Posted keepOut zone successfully.");
+    } // end postKeepOut
 
     // [HttpDelete("zones/in")]
-    // public async Task delKeepIn(string key)
+    // public async Task delKeepIn([FromBody] Zone requestBody)
     // {
     //     return key;
     // }
 
     // [HttpDelete("zones/out")]
-    // public async Task delKeepOut(string key)
+    // public async Task delKeepOut([FromBody] Zone requestBody)
     // {
     //     return key;
     // }
