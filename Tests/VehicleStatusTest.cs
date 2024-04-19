06568d64b248bea1ea44d4ec72d1c0b2508f236a
bodave <bodave28@att.net>
2024-04-19 14:19:48 -0700

    Added Unit Tests for vehicleStatusTest and created skeleton test classes for ZoneController and VehicleGeolocationController Test output still not functioning properly

--- Tests/VehicleStatusTest.cs
+++ Tests/VehicleStatusTest.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using RestSharp;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
+using System.Diagnostics;
 //using Database.Models;
 
 public class VehicleStatusTests
@@ -31,7 +32,20 @@ public class VehicleStatusTests
         //queryResult.Content.Should().NotBeNull();
         Assert.That(queryResult.Content, Is.Not.Null, "Response content is null");
     }
-/*
+
+    [Test]
+    public void Test1()
+    {
+        Debug.WriteLine("This is Debug.WriteLine");
+        Trace.WriteLine("This is Trace.WriteLine");
+        Console.WriteLine("This is Console.Writeline");
+        TestContext.WriteLine("This is TestContext.WriteLine");
+        TestContext.Out.WriteLine("This is TestContext.Out.WriteLine");
+        TestContext.Progress.WriteLine("This is TestContext.Progress.WriteLine");
+        TestContext.Error.WriteLine("This is TestContext.Error.WriteLine");
+        Assert.Pass();
+    }
+
     [Test]
     public void SetStatusStandby()
     {
@@ -45,11 +59,27 @@ public class VehicleStatusTests
         //Act
         var queryResult = client.Execute(restRequest);
         //Assert
-        queryResult.StatusCode.Should().Be(HttpStatusCode.OK);
-        queryResult.Content.Should().NotBeNull();
-        //Assert.Pass();
+        Assert.That(queryResult.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code is not OK");
+        Assert.That(queryResult.Content, Is.Not.Null, "Response content is null");
     }
-*/
+
+    [Test]
+    public void EmergencyStop()
+    {
+        //Arrange
+        var baseUrl = "http://localhost:5135/EmergencyStop";
+        RestClient client = new RestClient(baseUrl);
+        RestRequest restRequest = new RestRequest(baseUrl, Method.Get);
+        restRequest.AddJsonBody(new {
+            Key = "UAV"
+        });
+        //Act
+        var queryResult = client.Execute(restRequest);
+        //Assert
+        Assert.That(queryResult.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code is not OK");
+        Assert.That(queryResult.Content, Is.Not.Null, "Response content is null");
+    }
+
     [Test]
     public void SearchBooks()
     {
