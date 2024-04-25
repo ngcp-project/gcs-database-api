045b54261bcac0023fe96bff2b8eed6de0527c41
Skyhorizon2021 <locnguyen02152021@gmail.com>
2024-04-25 11:35:52 -0700

    Co-authored-by: Jason <GuyWhoCode@users.noreply.github.com>

--- Controllers/VehicleDataController.cs
+++ Controllers/VehicleDataController.cs
@@ -1,9 +1,10 @@
 using System.Net.Http.Headers;
+using System.Reflection;
 using Database.Models;
 using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 
-
+namespace Database.Controllers;
 public class VehicleDataController : ControllerBase
 {
     private ConnectionMultiplexer conn;
@@ -21,46 +22,48 @@ public class VehicleDataController : ControllerBase
         return _redis.StringGet("vehicleData");
     }
 
-    // [HttpPost("vehicleData")]
-    // public async Task<IActionResult> postVehicleData([FromBody] VehicleData requestBody)
-    // {
-    //     List<string> missingFields = new List<string>();
+    [HttpPost("vehicleData")]
+    public async Task<IActionResult> postVehicleData([FromBody] VehicleData requestBody)
+    {
+        List<string> missingFields = new List<string>();
 
-    //     Type type = typeof(VehicleData); // Replace ExampleModel with the respective model
-    //     PropertyInfo[] properties = type.GetProperties();
-    //     foreach (System.Reflection.PropertyInfo property in requestBody.GetType().GetProperties())
-    //     {
-    //         var value = property.GetValue(requestBody, null);
-    //         object defaultValue = null;
-    //         if (property.PropertyType == typeof(string))
-    //         {
-    //             defaultValue = null;
-    //         }
-    //         else if (property.PropertyType.IsValueType)
-    //         {
-    //             defaultValue = Activator.CreateInstance(property.PropertyType);
-    //         }
+        Type type = typeof(VehicleData); 
+        PropertyInfo[] properties = type.GetProperties();
+        
+        foreach (System.Reflection.PropertyInfo property in properties)
+        {
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
 
-    //         if (value?.Equals(defaultValue) == true || value == null)
-    //         {
-    //             missingFields.Add(property.Name);
-    //         }
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
 
-    //     }
-    //     // Iterates through every property in the model and checks if it is null or default value
+            if (missingFields.Count > 0)
+            {
+            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            }
 
-    //     if (missingFields.Count > 0)
-    //     {
-    //         return BadRequest("Missing fields: " + string.Join(", ", missingFields));
-    //     }
-    //     // If any field is missing, return a bad request
-    //     using (var sr = new StreamReader(Request.Body))
-    //     {
-    //         string content = await sr.ReadToEndAsync();
-    //         await _redis.StringSetAsync("vehicleData", content);
-    //     }
-    //     await _redis.StringSetAsync("vehicleData",requestBody.Vehicle); // Replace "example" with the respective database key
-    //     return Ok("done!");
-    //}
+        }
+        // Iterates through every property in the model and checks if it is null or default value
+
+        // If any field is missing, return a bad request
+        using (var sr = new StreamReader(Request.Body))
+        {
+            string content = await sr.ReadToEndAsync();
+            await _redis.StringSetAsync("vehicleData", content);
+        }
+        await _redis.StringSetAsync("vehicleData",requestBody.ToString()); // Replace "example" with the respective database key
+        return Ok("Posted VehicleData successfully!");
+    }
 
 }
