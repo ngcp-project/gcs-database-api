ed9649a57f1dc914a552cb4becc7f8a1628ff328
jhhocs <joshuaho1@cpp.edu>
2024-04-26 15:15:15 -0700

    Added EndpointReturn.cd model + Updated VehicleStatusController endpoint responses

--- Controllers/VehicleStatusController.cs
+++ Controllers/VehicleStatusController.cs
@@ -14,19 +14,20 @@ public class VehicleStatusController : ControllerBase
         gcs = redis.GetDatabase();
     }
 
-/*
---------------Posts and Gets----------------
-Body format for all posts and gets in this file:
-{
-    "Key": "[VehicleName]"
-}
-*/
+    /*
+    --------------Posts and Gets----------------
+    Body format for all posts and gets in this file:
+    {
+        "Key": "[VehicleName]"
+    }
+    */
 
     [HttpPost("SetStatusInUse")]
     public async Task<IActionResult> SetStatusInUse([FromBody] VehicleKey requestBody)
     {
         List<string> missingFields = new List<string>();
 
+        EndpointReturn endpointReturn = new EndpointReturn("", "", "");
         Type type = typeof(VehicleKey); // Replace ExampleModel with the respective model
         PropertyInfo[] properties = type.GetProperties();
         foreach (System.Reflection.PropertyInfo property in requestBody.GetType().GetProperties())
@@ -52,12 +53,14 @@ Body format for all posts and gets in this file:
 
         if (missingFields.Count > 0)
         {
-            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            endpointReturn.error = "Missing fields: " + string.Join(", ", missingFields);
+            return BadRequest(endpointReturn.ToString());
         }
         // If any field is missing, return a bad request
 
         await gcs.StringSetAsync($"{requestBody.Key}-status", "1"); // Replace "example" with the respective database key
-        return Ok("Status set to In Use");
+        endpointReturn.message = "In Use";
+        return Ok(endpointReturn.ToString());
     }
 
     [HttpPost("SetStatusStandby")]
@@ -65,6 +68,7 @@ Body format for all posts and gets in this file:
     {
         List<string> missingFields = new List<string>();
 
+        EndpointReturn endpointReturn = new EndpointReturn("", "", "");
         Type type = typeof(VehicleKey); // Replace ExampleModel with the respective model
         PropertyInfo[] properties = type.GetProperties();
         foreach (System.Reflection.PropertyInfo property in requestBody.GetType().GetProperties())
@@ -90,12 +94,14 @@ Body format for all posts and gets in this file:
 
         if (missingFields.Count > 0)
         {
-            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            endpointReturn.error = "Missing fields: " + string.Join(", ", missingFields);
+            return BadRequest(endpointReturn.ToString());
         }
         // If any field is missing, return a bad request
 
         await gcs.StringSetAsync($"{requestBody.Key}-status", "2"); // Replace "example" with the respective database key
-        return Ok("Status set to Standby");
+        endpointReturn.message = "Standby";
+        return Ok(endpointReturn.ToString());
     }
 
     [HttpPost("EmergencyStop")]
@@ -103,6 +109,7 @@ Body format for all posts and gets in this file:
     {
         List<string> missingFields = new List<string>();
 
+        EndpointReturn endpointReturn = new EndpointReturn("", "", "");
         Type type = typeof(VehicleKey); // Replace ExampleModel with the respective model
         PropertyInfo[] properties = type.GetProperties();
         foreach (System.Reflection.PropertyInfo property in requestBody.GetType().GetProperties())
@@ -128,27 +135,32 @@ Body format for all posts and gets in this file:
 
         if (missingFields.Count > 0)
         {
-            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            endpointReturn.error = "Missing fields: " + string.Join(", ", missingFields);
+            return BadRequest(endpointReturn.ToString());
         }
         // If any field is missing, return a bad request
 
         await gcs.StringSetAsync($"{requestBody.Key}-status", "3"); // Replace "example" with the respective database key
-        return Ok("Vehicle Emergency Stopped");
+        endpointReturn.message = "Emergency Stopped";
+        return Ok(endpointReturn.ToString());
     }
 
     [HttpGet("GetVehicleStatus")]
-    public async Task<string> GetVehicleStatus(){
+    public async Task<string> GetVehicleStatus()
+    {
         int num = 0; //Default of 0, database value will be read into this variable
-        using (var sr = new StreamReader(Request.Body)){
+        using (var sr = new StreamReader(Request.Body))
+        {
             string vehicleData = await sr.ReadToEndAsync();
             VehicleKey? vehicleKey = JsonSerializer.Deserialize<VehicleKey>(vehicleData);
             string key = $"{vehicleKey?.Key}-status";
-            num = (int) await gcs.StringGetAsync(key);
+            num = (int)await gcs.StringGetAsync(key);
         }
         string output; //output string, will be returned. 
         //Switch block to conert int value in database to string value for enum. 
-        switch (num){
-            case 1: 
+        switch (num)
+        {
+            case 1:
                 output = "In Use";
                 break;
             case 2:
