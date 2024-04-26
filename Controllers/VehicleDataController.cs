9b0c4352b24cddb9e831d0caaa456a583f3f0eda
Loc N <101013858+Skyhorizon2021@users.noreply.github.com>
2024-04-25 18:12:21 -0700

    Vehicle post endpoint (#43)
    * added post endpoint
    
    * added get and post endpoint for vehicle data controller
    
    * added checks for post endpoint
    
    * added checks for post endpoint
    
    * added tostring method for the target coord and the search area
    
    * Co-authored-by: Jason <GuyWhoCode@users.noreply.github.com>
    
    * update vehicledata
    
    * tested endpoint successfully and use serialize for toString method
    
    * tested post and get endpoint for vehicle data

--- Controllers/VehicleDataController.cs
+++ Controllers/VehicleDataController.cs
@@ -1,15 +1,91 @@
-namespace Database.Controllers
+using System.Net.Http.Headers;
+using System.Reflection;
+using Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Database.Controllers;
+public class VehicleDataController : ControllerBase
 {
-    public class VehicleDataController
+    private ConnectionMultiplexer conn;
+    private readonly IDatabase _redis;
+
+    public VehicleDataController()
     {
-        // static void Main(string[] args)
-        // {
-        //     using(var client = new HttpClient())
-        //     {
-        //         var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
-        //         var result = client.GetAsync(endpoint).Result;
-        //         var json = result.Content.ReadAsStringAsync().Result;
-        //     }
-        // }
+        conn = DBConn.Instance().getConn();
+        _redis = conn.GetDatabase();
     }
+    [HttpGet("vehicleData")]
+    public IActionResult getVehicleData([FromBody] VehicleKey requestBody)
+    {
+        List<string> missingFields = new List<string>();
+
+        Type type = typeof(VehicleKey); 
+        PropertyInfo[] properties = type.GetProperties();
+        
+        foreach (System.Reflection.PropertyInfo property in properties)
+        {
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
+
+            if (missingFields.Count > 0)
+            {
+            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            }
+        }
+        // return vehicle data
+        Console.WriteLine(requestBody.Key+"-geo");
+        return Ok(_redis.StringGet(requestBody.Key+"-geo").ToString());
+
+    }
+
+    [HttpPost("vehicleData")]
+    public async Task<IActionResult> postVehicleData([FromBody] VehicleData requestBody)
+    {
+        List<string> missingFields = new List<string>();
+
+        Type type = typeof(VehicleData); 
+        PropertyInfo[] properties = type.GetProperties();
+        
+        foreach (System.Reflection.PropertyInfo property in properties)
+        {
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
+
+            if (missingFields.Count > 0)
+            {
+            return BadRequest("Missing fields: " + string.Join(", ", missingFields));
+            }
+
+        }
+        await _redis.StringSetAsync(requestBody.vehicleName+"-geo",requestBody.ToString()); 
+        return Ok("Posted VehicleData successfully!");
+    }
+
 }
---
+++ Models/VehicleData.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Database.Models;
+
+
+namespace Database.Models
+{
+    public class VehicleData
+    {
+        public String vehicleName { get; set; }
+        public bool IsManual { get; set; } = true;
+        public Coordinate Target { get; set; }
+       public Coordinate[] SearchArea { get; set; }
+
+        public String localIP { get; set; }
+
+    public override string ToString() 
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+}
+}
