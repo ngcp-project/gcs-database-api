02a31152ba732bf7269deb4b58dcece11bf03d81
jhhocs <joshuaho1@cpp.edu>
2024-05-20 20:53:40 -0700

    Added POST CurrentStage endpoint

--- Controllers/MissionInfoController.cs
+++ Controllers/MissionInfoController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 using Database.Models;
 using System.Reflection;
+using System.Text.Json;
+using System.Linq;
 
 
 namespace Database.Controllers;
@@ -98,7 +100,7 @@ public class MissionInfoController : ControllerBase
         MissionInfo missionInfo = new MissionInfo
         {
             missionName = requestBody.missionName,
-            currentStageId = requestBody.stageName,
+            // currentStageId = requestBody.stageName,
             stages = stages.ToArray()
         };
         // Initializes new MissionInfo object with a MissionStage attached to it
@@ -108,5 +110,64 @@ public class MissionInfoController : ControllerBase
         endpointReturn.message = "Posted MissionInfo";
         return Ok(endpointReturn.ToString());
     }
-    
+
+    [HttpPost("CurrentStage")]
+    public async Task<IActionResult> SetCurrentStage([FromBody] CurrentStagePOST requestBody)
+    {
+        List<string> missingFields = new List<string>();
+
+        EndpointReturn endpointReturn = new EndpointReturn("", "", "");
+        Type type = typeof(CurrentStagePOST);
+        PropertyInfo[] properties = type.GetProperties();
+
+        foreach (System.Reflection.PropertyInfo property in properties)
+        {
+            var value = property.GetValue(requestBody, null);
+            object defaultValue = null;
+            if (property.PropertyType == typeof(string))
+            {
+                defaultValue = null;
+            }
+
+            else if (property.PropertyType.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(property.PropertyType);
+            }
+
+            if (value?.Equals(defaultValue) == true || value == null)
+            {
+                missingFields.Add(property.Name);
+            }
+            if (missingFields.Count > 0)
+            {
+                endpointReturn.error = "Missing fields: " + string.Join(", ", missingFields);
+                return BadRequest(endpointReturn.ToString());
+            }
+        }
+
+        // Pull MissionInfo from database and update currentStageId
+        try
+        {
+            MissionInfo missionInfo = JsonSerializer.Deserialize<MissionInfo>(gcs.StringGet(requestBody.missionName));
+            foreach (MissionStage stage in missionInfo.stages)
+            {
+                if (stage.stageName == requestBody.currentStage)
+                {
+                    missionInfo.currentStageId = requestBody.currentStage;
+                    await gcs.StringSetAsync(requestBody.missionName, missionInfo.ToString());
+
+                    endpointReturn.message = "Posted CurrentStage";
+                    return Ok(endpointReturn.ToString());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            endpointReturn.error = e.Message;
+            return BadRequest(endpointReturn.ToString());
+        }
+
+        endpointReturn.error = "MissionStage does not exist";
+        return BadRequest(endpointReturn.ToString());
+    }
 }
---
+++ Models/CurrentStagePOST.cs
@@ -0,0 +1,10 @@
+namespace Database.Models
+{
+
+    public class CurrentStagePOST
+    {
+
+        public string missionName { get; set; }
+        public string currentStage { get; set; }
+    }
+}
