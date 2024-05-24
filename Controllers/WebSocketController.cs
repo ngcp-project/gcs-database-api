d2b3a62218f470421539d2388d00ae050962a400
jhhocs <86144532+jhhocs@users.noreply.github.com>
2024-05-23 22:38:05 -0700

    Websocket (#57)
    * Save data to database
    
    * Added vehicle data validation
    
    * removed sln file
    
    * Fixed log comment
    
    * small changes
    
    * Fixed inputing data to database
    
    * Added in code documentation comments
    
    * changed code to only create 1 rabbitmq listener per vehicle
    
    * bug fixes
    
    * removed log messages
    
    * Websocket connections now close when disconnected
    
    * cleanup

--- Controllers/WebSocketController.cs
+++ Controllers/WebSocketController.cs
@@ -5,6 +5,7 @@ using StackExchange.Redis;
 using Database.Handlers;
 using System.Text.Json;
 using Database.Models;
+using System.Collections.Concurrent;
 
 namespace Database.Controllers
 {
@@ -21,6 +22,10 @@ namespace Database.Controllers
         private readonly ILogger<WebSocketController> _logger;
         private readonly IDatabase _redis;
         private readonly RabbitMqConsumer _rabbitmq;
+
+        static readonly ConcurrentDictionary<string, List<WebSocket>> _wsConnections = new ConcurrentDictionary<string, List<WebSocket>>();
+
+        static int connId = 0;
 
         public WebSocketController(
             ILogger<WebSocketController> logger,
@@ -47,49 +52,79 @@ namespace Database.Controllers
             {
                 // Accept the WebSocket connection & establish queue name
                 using WebSocket ws = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                string queueName = $"telemetry_{vehicleName.ToLower()}";
 
-                _logger.Log(LogLevel.Information, $"\nWebsocket connection established with {vehicleName.ToUpper()}!\n");
-                _logger.Log(LogLevel.Information, "\nQueue: " + queueName);
-                _rabbitmq.CreateConsumer(queueName);
+                // Update number of connections for given vehicle
+                string connKey = Interlocked.Increment(ref connId).ToString();
+
+                // 
+                bool newConsumer = true;
+
+                // Group WebSocket connections by vehicle 
+                _wsConnections.AddOrUpdate(vehicleName.ToLower(), new List<WebSocket>() { ws }, (key, oldValue) =>
+                {
+                    newConsumer = false;
+                    oldValue.Add(ws);
+                    return oldValue;
+                });
+
+                _logger.Log(LogLevel.Information, $"\nWebsocket connection established with {vehicleName.ToUpper()}{connKey}!\n");
 
                 // Use a CancellationTokenSource to control the loop
-                CancellationTokenSource tokenSource = new();
+                CancellationTokenSource tokenSource = null;
 
-                // Start the RabbitMQ consumer in a separate task
-                Task task = Task.Run(() => ListenToRabbitMq(ws, queueName, tokenSource.Token, vehicleName));
+                // Start RabbitMQ consumer if it is the first WebSocket connection
+                if (newConsumer)
+                {
+                    string queueName = $"telemetry_{vehicleName.ToLower()}";
+                    _logger.Log(LogLevel.Information, "\nQueue: " + queueName);
+                    _rabbitmq.CreateConsumer(queueName);
+
+                    tokenSource = new();
+
+                    // Start the RabbitMQ consumer in a separate task
+                    Task task = Task.Run(() => ListenToRabbitMq(ws, queueName, tokenSource.Token, vehicleName));
+                }
 
                 byte[] buffer = new byte[1024 * 4];
-                WebSocketReceiveResult result;
+                WebSocketReceiveResult result = null;
 
                 // Stop listening to the specific client when specified
                 do
                 {
-                    result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    try
+                    {
+                        result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    }
+                    catch (WebSocketException e)
+                    {
+                        break;
+                    }
                     Console.Write("-");
                     Thread.Sleep(100);
 
                 } while (!result.CloseStatus.HasValue);
 
-                tokenSource.Cancel();
+                // if (tokenSource != null)
+                // {
+                // tokenSource.Cancel();
+                // _rabbitmq.StopConsuming();
+                // }
 
-                _logger.Log(LogLevel.Error, "Websocket connection aborted!");
-                _rabbitmq.StopConsuming();
-
+                CloseWebSocketConnection(vehicleName, ws, _logger, connKey);
             }
             else HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
         }
 
-/**
-        * <summary>
-        *   Establish a WebSocket connection to retrieve vehicle telemetry from backend
-        * </summary>
-        * <param name="ws">Open WebSocket connection</param>
-        * <param name="queueName">RabbitMQ consumer queueName. Format: telemetry_vehiclename (all lowercase)</param>
-        * <param name="cancellationToken">Token used to cancel or dispose of RabbitMQ consumer</param>
-        */
+        /**
+                * <summary>
+                *   Establish a WebSocket connection to retrieve vehicle telemetry from backend
+                * </summary>
+                * <param name="ws">Open WebSocket connection</param>
+                * <param name="queueName">RabbitMQ consumer queueName. Format: telemetry_vehiclename (all lowercase)</param>
+                * <param name="cancellationToken">Token used to cancel or dispose of RabbitMQ consumer</param>
+                */
         private async Task ListenToRabbitMq(WebSocket ws, string queueName, CancellationToken cancellationToken, string vehicleName)
-        {   
+        {
             // Add a callback function to RabbitMQ
             // NOTE: This will be triggered whenever a RabbitMQ message is received
             _rabbitmq.Consumer.Received += async (channel, eventArgs) =>
@@ -101,10 +136,12 @@ namespace Database.Controllers
 
                 _logger.Log(LogLevel.Information, inputString);
 
-                try {
+                try
+                {
                     vehicleData = JsonSerializer.Deserialize<Vehicle>(inputString);
                 }
-                catch (Exception e) {
+                catch (Exception e)
+                {
                     _logger.Log(LogLevel.Error, "Error: " + e.Message);
                     return;
                 }
@@ -116,14 +153,20 @@ namespace Database.Controllers
                 _logger.Log(LogLevel.Information, "Saving vehicle data to database!");
                 _redis.StringSet(vehicleKey, inputString);
 
-                _logger.Log(LogLevel.Information, "Sending WebSocket message...");
+                _logger.Log(LogLevel.Information, "Sending WebSocket messages...");
 
-                await ws.SendAsync(
-                    new ArraySegment<byte>(eventArgs.Body.ToArray()),
-                    WebSocketMessageType.Text,
-                    true,
-                    CancellationToken.None
-                );
+                _wsConnections.TryGetValue(vehicleName, out List<WebSocket> wsList);
+
+                // Send vehicle data to all relevant WebSocket connections
+                foreach (WebSocket wsConn in wsList)
+                {
+                    await wsConn.SendAsync(
+                        new ArraySegment<byte>(eventArgs.Body.ToArray()),
+                        WebSocketMessageType.Text,
+                        true,
+                        CancellationToken.None
+                    );
+                }
             };
 
             _rabbitmq.StartConsuming(queueName);
@@ -139,5 +182,14 @@ namespace Database.Controllers
             }
         }
 
+        private static void CloseWebSocketConnection(string vehicleName, WebSocket ws, ILogger<WebSocketController> _logger, string connKey)
+        {
+            _wsConnections.TryGetValue(vehicleName.ToLower(), out List<WebSocket> wsList);
+            wsList.Remove(ws);
+            _logger.Log(LogLevel.Error, "Websocket connection closed for " + vehicleName.ToUpper() + connKey + "!\n");
+
+            // Close the WebSocket connection
+            ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Connection closed", CancellationToken.None);
+        }
     }
 }
